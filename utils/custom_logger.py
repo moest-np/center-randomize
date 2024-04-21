85e72be946782ae78911da27c9c2c1ddb7cbdbeb
yubinkarki <iamyubinkarki@gmail.com>
2024-04-21 09:32:53 +0545

    config: use dictConfig to tweak logger

--- utils/custom_logger.py
+++ utils/custom_logger.py
@@ -1,25 +1,61 @@
 """Custom logging module with some formatting."""
 
 import sys
-import logging
+import logging.config
+from os.path import abspath, dirname, join
 
 
-def setup_logger(name: str) -> logging.Logger:
-    """Configure and return a logger."""
+MAX_LOGS_SIZE: int = 500000
+ROOT_DIR: str = abspath(dirname(dirname(__file__)))
+LOGS_TARGET: str = join(ROOT_DIR, "logs", "custom_logs.log")
+CUSTOM_FILE_HANDLER_PATH = "utils.custom_file_handler.CustomFileHandler"
 
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
 
-    # Create console handler and set level to info
-    ch = logging.StreamHandler(sys.stdout)
-    ch.setLevel(logging.INFO)
+LOGGING_CONFIG: dict = {
+    "version": 1,
+    "formatters": {
+        "standard": {
+            "datefmt": "%y-%m-%d %H:%M:%S",
+            "format": "🚀 %(asctime)s - %(name)s - %(levelname)s - %(message)s \n",
+        },
+        "error": {
+            "datefmt": "%y-%m-%d %H:%M:%S",
+            "format": "❌ %(asctime)s - %(name)s - %(levelname)s - %(message)s \n",
+        },
+    },
+    "handlers": {
+        "file_error": {
+            "mode": "a",
+            "level": "ERROR",
+            "encoding": "utf-8",
+            "formatter": "error",
+            "filename": LOGS_TARGET,
+            "class": CUSTOM_FILE_HANDLER_PATH,
+        },
+        "console_info": {
+            "level": "INFO",
+            "stream": sys.stdout,
+            "formatter": "standard",
+            "class": "logging.StreamHandler",
+        },
+        "console_error": {
+            "level": "ERROR",
+            "stream": sys.stderr,
+            "formatter": "error",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "": {
+            "level": "INFO",
+            "propagate": True,
+            "handlers": ["file_error", "console_info", "console_error"],
+        }
+    },
+}
 
-    formatter = logging.Formatter(
-        "🚀 %(asctime)s - %(name)s - %(levelname)s - %(message)s", "%y-%m-%d %H:%M:%S"
-    )
 
-    ch.setFormatter(formatter)
-
-    logger.addHandler(ch)
-
-    return logger
+# Call this once to initialize logger
+def configure_logging() -> None:
+    """Pass the logging configuration to logger."""
+    logging.config.dictConfig(LOGGING_CONFIG)
